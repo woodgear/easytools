e433d169cc2760d2df40108520cc4809af575c77
wucong <1875486458@qq.com>
2020-10-19 00:10:47 +0800

    implement cli

---
+++ src/auxre-zip-decoder/src/lib.ts
@@ -0,0 +1,94 @@
+import { fstat } from "fs-extra";
+import * as iconv from "iconv-lite"
+import JSZip = require("jszip");
+import { async } from "walkdir";
+
+export function ping(): string {
+    return "pong"
+}
+
+export function transformGbk(origin: string): string {
+    const str = iconv.encode(origin, 'GBK');
+    const out = iconv.decode(Buffer.from(str), 'utf8');
+    return out;
+}
+
+// data struct represent a zip file
+export interface ZipObj {
+    items: (ZipFileItem | ZipFolderItem)[];
+}
+
+export type ZipFileItem = { name: string, content: Base64String }
+export type ZipFolderItem = { name: string, content: ZipObj };
+export type ZipItem = ZipFileItem | ZipFolderItem;
+export type Base64String = string;
+
+function isZipFolderItem(zipItem: ZipItem): zipItem is ZipFolderItem {
+    return !(typeof zipItem.content === "string");
+}
+
+export async function zipToZipObj(zipFile: Base64String): Promise<ZipObj> {
+
+    const zip = new JSZip();
+    await zip.loadAsync(zipFile, { base64: true })
+    const zipObj: ZipObj = { items: [] };
+    // use this cache to access nested object in zipObj
+    const zipObjDirCache: { [name: string]: ZipObj } = { "": zipObj };
+    const files: ([string, JSZip.JSZipObject])[] = [];
+
+    zip.forEach((path, file) => {
+        files.push([path, file])
+    });
+    for (let [path, file] of files) {
+        if (file.dir) {
+            path = path.slice(0, -1)
+            console.assert(!!!zipObjDirCache[path], "zipObjDirCache should only be access once");
+            // if file is dir path should be aa/xxx/ we need to remove the end of /
+            const paths = path.split("/");
+            const dirName = paths.pop();
+            const parentPath = paths.length === 0 ? "" : paths.join("/");
+            zipObjDirCache[path] = { items: [] }
+            zipObjDirCache[parentPath].items.push({ name: dirName, content: zipObjDirCache[path] });
+        } else {
+            const paths = path.split("/");
+            const fileName = paths.pop();
+            const dirPath = paths.length === 0 ? "" : paths.join("/");
+            //TODO 当路径中有斜杠时会出问题
+            const content = await file.async("base64");
+            zipObjDirCache[dirPath].items.push({ name: fileName, content });
+        }
+    }
+    return zipObj;
+}
+
+export async function zipObjToZip(zipObj: ZipObj): Promise<Base64String> {
+    const zip = new JSZip();
+    buildZip(zip, zipObj);
+    return (await zip.generateAsync({ type: "base64" })) as Base64String;
+
+    function buildZip(zip: JSZip, zipObj: ZipObj) {
+        for (const zipItem of zipObj.items) {
+            if (isZipFolderItem(zipItem)) {
+                buildZip(zip.folder(zipItem.name), zipItem.content);
+            } else {
+                zip.file(zipItem.name, zipItem.content, { base64: true, binary: true });
+            }
+        }
+    }
+}
+
+function fixEncode(origin: string): string {
+    return transformGbk(origin);
+}
+
+export function fixZipObj(zipObj: ZipObj) {
+    for (const item of zipObj.items) {
+        if (isZipFolderItem(item)) {
+            item.name = fixEncode(item.name);
+            fixZipObj(item.content)
+        } else {
+            item.name = fixEncode(item.name);
+        }
+    }
+    return zipObj;
+}
---
+++ src/auxre-zip-decoder/src/main.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs-extra"
+import { fixZipObj, zipObjToZip, zipToZipObj } from "./lib";
+import rename from 'node-rename-path';
+
+async function fixZip(originPath: string, newPath: string) {
+    if (!fs.existsSync(originPath)) {
+        console.error(`could not find ${originPath}`)
+        return -1
+    }
+    const rawZip = await fs.readFile(originPath, "base64");
+    const zipObj = await zipToZipObj(rawZip)
+    const fixedZipObj = fixZipObj(zipObj);
+    const fixedRawZip = await zipObjToZip(fixedZipObj);
+    await fs.writeFileSync(newPath, fixedRawZip, "base64");
+}
+
+async function main() {
+    const originPath = process.argv[1];
+    console.log('start fix encoding of', originPath);
+    const newPath = rename(originPath, (pathObj) => {
+        return { name: `${pathObj.name}.fixed` }
+    })
+    await fixZip(originPath, newPath);
+    console.log('end fix encoding new path is', originPath);
+}
+
+main();
